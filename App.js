5e241448182c0872ce4a0bc2e17ec4e30aefad47
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2021-06-14 11:12:48 +0700

    multiple text content

--- App.js
+++ App.js
@@ -8,10 +8,58 @@ export default function App() {
       flex: 1, 
       flexDirection: 'column',
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'stretch'
       }}>
-      <Text style={{fontSize: 30, fontWeight: 'bold'}}>Stream I.T. Consulting Ltd.</Text>
-      <Text style={{fontSize: 30, fontWeight: 'bold'}}>Stream I.T. Consulting Ltd.</Text>
+      <Text style={{
+        fontSize: 30, 
+        fontWeight: 'bold',
+        backgroundColor: 'purple',
+        flex: 1,
+        textAlign: 'center',
+        textAlignVertical: 'center'
+        }}>
+          Stream I.T. Consulting Ltd.
+        </Text>
+      <Text style={{
+        fontSize: 30, 
+        fontWeight: 'bold',
+        backgroundColor: 'green',
+        flex: 1,
+        textAlign: 'center',
+        textAlignVertical: 'center'
+        }}>
+          Stream I.T. Consulting Ltd.
+        </Text>
+      <Text style={{
+        fontSize: 30, 
+        fontWeight: 'bold',
+        backgroundColor: 'white',
+        flex: 1,
+        textAlign: 'center',
+        textAlignVertical: 'center'
+        }}>
+          Stream I.T. Consulting Ltd.
+        </Text>
+      <Text style={{
+        fontSize: 30, 
+        fontWeight: 'bold',
+        backgroundColor: 'blue',
+        flex: 1,
+        textAlign: 'center',
+        textAlignVertical: 'center'
+        }}>
+          Stream I.T. Consulting Ltd.
+        </Text>
+      <Text style={{
+        fontSize: 30, 
+        fontWeight: 'bold',
+        backgroundColor: 'orange',
+        flex: 1,
+        textAlign: 'center',
+        textAlignVertical: 'center'
+        }}>
+          Stream I.T. Consulting Ltd.
+        </Text>        
     </View>
   )
 }
