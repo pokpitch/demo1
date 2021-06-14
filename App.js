72acebf9ddfa830e55f0c7cc1af018064620b3e7
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2021-06-14 10:52:20 +0700

    playing with flex property

--- App.js
+++ App.js
@@ -3,8 +3,15 @@ import { View, Text } from 'react-native'
 
 export default function App() {
   return (
-    <View style={{backgroundColor: 'green', flex: 0.5}}>
-      <Text style={{fontSize: 30}}>Stream I.T. Consulting Ltd.</Text>
+    <View style={{
+      backgroundColor: '#FF0', 
+      flex: 1, 
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+      }}>
+      <Text style={{fontSize: 30, fontWeight: 'bold'}}>Stream I.T. Consulting Ltd.</Text>
+      <Text style={{fontSize: 30, fontWeight: 'bold'}}>Stream I.T. Consulting Ltd.</Text>
     </View>
   )
 }
