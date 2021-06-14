a25487dab4bddff5fbf1ff79d6ff95b169e73143
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2021-06-14 10:46:59 +0700

    change font size

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { View, Text } from 'react-native'
 
 export default function App() {
   return (
-    <View style={{backgroundColor:'green', flex:0.5}}>
-      <Text>Stream I.T. Consulting Ltd.</Text>
+    <View style={{backgroundColor: 'green', flex: 0.5}}>
+      <Text style={{fontSize: 30}}>Stream I.T. Consulting Ltd.</Text>
     </View>
   )
 }
