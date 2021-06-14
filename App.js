8a0fc455657d1cf3d8bf5a3136f3b05ab35a3834
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2021-06-14 10:44:14 +0700

    change bg color

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { View, Text } from 'react-native'
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello Stream I.T. Consulting Ltd.</Text>
+    <View style={{backgroundColor:'green', flex:0.5}}>
+      <Text>Stream I.T. Consulting Ltd.</Text>
     </View>
   )
 }
